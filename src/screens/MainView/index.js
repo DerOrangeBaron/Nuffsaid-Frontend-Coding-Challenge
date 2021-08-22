32f927999761f64229b74b1d56c65561c3e315f2
Der Orange Baron <mail@nicolaslopez.uy>
2021-08-22 04:20:01 -0300

    Refactored code

---
+++ src/screens/MainView/index.js
@@ -0,0 +1,59 @@
+import React, { useState, useReducer, useEffect, useRef, useCallback } from 'react'
+import Header from '../../components/Header'
+import Button from '../../components/Button'
+import Api from '../../api'
+
+const MainView = () => {
+    const [, forceUpdate] = useReducer(x => !x, true);
+    const [api, setApi] = useState( null )
+
+    const messageReducer = (state, action) => { 
+        let messages = [];
+        if (action.type === 'add') {
+            messages = [
+                ...state.messages,
+                action.message,
+            ];
+        }
+        console.log(messages)     
+        return {messages}
+    }
+    const [state, dispatch] = useReducer(messageReducer, {messages:[]});
+
+    
+
+    //Create the API
+    useEffect(() => {
+        setApi(new Api({
+            messageCallback:  (message) => dispatch({message, type: 'add'})
+        }))
+    }, [])
+
+    // Start the API before created
+    const isStart = useRef(true);
+    useEffect(() => {
+        if (!isStart.current) api.start()
+        else isStart.current = false
+    }, [api])
+
+
+    //Start or stop the API
+    const apiButtonOption = (api && api.isStarted()) ? 'stop' : 'start';
+    const handleApiButton = () => {
+        api[apiButtonOption]();
+        forceUpdate()
+    }
+
+    return (
+        <div>
+          <Header>
+            {/* {this.renderButton()} */}
+            <Button text={apiButtonOption} action={handleApiButton} />
+            <Button text="Clear" action={() => dispatch({type: 'clear'})}/>
+          </Header>
+
+        </div>
+      )
+}
+
+export default MainView;
